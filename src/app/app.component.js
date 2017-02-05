223c5a70238bc6d61533734e873e67807a7b290d
sordonia120446 <samuel.ordonia@gmail.com>
2017-02-05 09:19:31 -0800

    added in for loop iteration through heroes enum

---
+++ src/app/app.component.js
@@ -0,0 +1,55 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+// deprecated Component declaration
+// @Component({
+//   selector: 'my-app',
+//   template: `<h1>Hello {{name}}</h1>`,
+// })
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+var AppComponent = (function () {
+    function AppComponent() {
+        this.name = 'Angular';
+        this.title = 'Tour of Heroes';
+        this.heroes = HEROES;
+        this.hero = {
+            id: 1,
+            name: 'Windstorm'
+        };
+    }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            template: "\n  \t<h1>{{title}}</h1>\n  \t<h2>{{hero.name}} details!</h2>\n  \t<ul class=\"heroes\">\n  \t\t<li *ngFor=\"let hero of heroes\">\n  \t\t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t</li>\n\t</ul>\n  \t<div><label>ID: </label>{{hero.id}}</div>\n  \t<div>\n  \t\t<label>name: </label>\n  \t\t<input [(ngModel)] = \"hero.name\" placeholder=\"name\">\n\t</div>\n  "
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+var HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+//# sourceMappingURL=app.component.js.map
