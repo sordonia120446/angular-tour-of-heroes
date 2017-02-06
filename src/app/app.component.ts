fc2772b23fde660ca69bc37983e4e5369d27d172
sordonia120446 <samuel.ordonia@gmail.com>
2017-02-05 21:31:27 -0800

    added in styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,17 +17,66 @@ export class Hero {
   template: `
   	<h1>{{title}}</h1>
   	<h2>{{hero.name}} details!</h2>
-  	<u1 class="heroes">
+  	<ul class="heroes">
   		<li *ngFor="let hero of heroes">
-  			<!-- each hero goes here -->
+  			<span class="badge">{{hero.id}}</span> {{hero.name}}
 		</li>
-	</u1>
+	</ul>
   	<div><label>ID: </label>{{hero.id}}</div>
   	<div>
   		<label>name: </label>
   		<input [(ngModel)] = "hero.name" placeholder="name">
 	</div>
-  `
+  `, 
+  styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
   // template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
 })
 
@@ -54,6 +103,7 @@ const HEROES: Hero[] = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
 ];
+
 
 
 
